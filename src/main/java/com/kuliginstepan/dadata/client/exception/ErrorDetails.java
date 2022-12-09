7ba2b190761d3b9ec30feedfd068203902420622
Stepan Kuligin <kuligin_stepan@mail.ru>
2022-12-09 18:23:39 +0300

    Update dependencies

--- src/main/java/com/kuliginstepan/dadata/client/exception/ErrorDetails.java
+++ src/main/java/com/kuliginstepan/dadata/client/exception/ErrorDetails.java
@@ -1,8 +1,12 @@
 package com.kuliginstepan.dadata.client.exception;
 
+import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 
 @Value
+@Builder
+@Jacksonized
 public class ErrorDetails {
 
     String family;
